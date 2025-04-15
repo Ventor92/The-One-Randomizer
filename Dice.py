7ccfd0d07119cc47cae05feaa72580428116a2fb
Ventor92 <ventor92@gmail.com>
2025-04-15 17:48:27 +0200

    TOR-001 Dices

---
+++ Dice.py
@@ -0,0 +1,36 @@
+import random
+from typing import List
+
+from enum import Enum
+
+class DiceType(Enum):
+    D2 = 2
+    D3 = 3
+    D4 = 4
+    D6 = 6
+    D8 = 8
+    D10 = 10
+    D12 = 12
+    D20 = 20
+    D100 = 100
+
+class Dice:
+    def __init__(self, diceType=DiceType.D6):
+        self.__diceType = diceType
+        self.__lastRoll = [0]
+
+    def _roll(self, numDice: int) -> List[int]:
+        roll = [random.randint(1, self.__diceType.value) for _ in range(numDice)]
+        roll.sort()
+        self.__lastRoll = roll
+        return roll
+    
+    def getLastRoll(self) -> List[int]:
+        return self.__lastRoll
+    
+    def getType(self) -> DiceType:
+        return self.__diceType
+    
+    def roll(self, numDice: int) -> List[int]:
+        roll:List[int] = self._roll(numDice)
+        return roll
---
+++ DiceTheOneRing.py
@@ -0,0 +1,37 @@
+from enum import Enum
+from Dice import Dice, DiceType
+
+class DiceTheOneRingType(Enum):
+    FEAT = DiceType.D12
+    SUCCESS = DiceType.D6
+
+class DiceFeatType(Enum):
+    NORMAL = 1
+    ILL = 2
+    FAVOURED = 3
+
+class DiceTheOneRing(Dice):
+    def __init__(self, type: DiceTheOneRingType):
+        super().__init__(type.value)  # wywołujemy konstruktor klasy bazowej
+
+    @staticmethod
+    def log(type: DiceTheOneRingType, roll: int):
+        if type == DiceTheOneRingType.SUCCESS and roll == 6:
+            string:str =  " — Elfi Znak τ!"
+        elif type == DiceTheOneRingType.FEAT and roll == 11:
+            string:str =  " — Oko Saurona!"
+        elif type == DiceTheOneRingType.FEAT and roll == 12:
+            string:str =  " — Runa Gandalfa!"
+        else:
+            string:str =  ""
+
+        print(f"Typ: {type.name} Wynik: {roll}{string}")
+
+    def roll(self, numDice = 1):
+        rolls = self._roll(numDice)
+        for roll in rolls:
+            nativeType = self.getType()
+            type:DiceTheOneRingType = DiceTheOneRingType(nativeType)
+            self.log(type, roll)
+
+        return rolls
